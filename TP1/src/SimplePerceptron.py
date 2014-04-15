640addf2e95302671986d4cd9154ba48b4278d95
marian.sabianaa <marian.sabianaa@6c33e5c6-7162-ad23-4877-bb0ec75854d9>
2014-04-15 00:07:22 +0000

    sigue sin andar pero por lo menos no da cosas asquerosas

--- TP1/src/SimplePerceptron.py
+++ TP1/src/SimplePerceptron.py
@@ -21,6 +21,14 @@ class SimplePerceptron:
             for learningData in shuffledLearningDatas:
                 evaluationVector = self.applySignFunction(learningData.input, self.matrix)
                 iterationError = np.subtract(learningData.expectedOutput, evaluationVector)
+                #'''Used for debugging
+                print 'input: ' + str(learningData.input)
+                print 'matrix: ' + str(self.matrix)
+                print 'evaluationVector: ' + str(evaluationVector)
+                print 'expectedOuput: ' + str(learningData.expectedOutput)
+                print 'iteration error: ' + str(iterationError)
+                print '---------------'
+                #'''
                 iterationErrors.append(iterationError)
                 
                 #---> Line below does: 
@@ -30,10 +38,12 @@ class SimplePerceptron:
                 deltaMatrix = np.dot(self.parameters.etta, np.dot(transposeInput, iterationErrorMatrix)) 
                 
                 self.matrix = np.add(self.matrix,deltaMatrix) #algoritmo incremental
-                self.showEvolution()
+                #self.showEvolution()
+                
         
             epsilonOnIteration = 0.5 * self.sumSquaredNorms(iterationErrors)
             iteratedEpochs += 1
+            self.testWhatWasLearnt(iteratedEpochs)
         
     def sumSquaredNorms(self, errors):
         return sum([self.squaredNorm(e) for e in errors])
@@ -47,26 +57,39 @@ class SimplePerceptron:
         
         columnsAsRows = matrix.transpose() #es un truquito para poder agarrar rapido las columnas, no se si se puede hacer de otra forma mas eficiente
         vectorDotMatrix = []
-        for columnIndex in range(0, columnsAsRows.shape[0]-1):
+        #print '----> shape 0: ' + str(columnsAsRows.shape[0])
+        for columnIndex in range(0, columnsAsRows.shape[0]):
             matrixVector = columnsAsRows[columnIndex].transpose()
-            vectorDotMatrix.append(np.dot(vector, matrixVector))
+            vectorialProduct = np.dot(vector, matrixVector)
+            vectorDotMatrix.append(vectorialProduct)
+            ''' Used for debugging
+            print '-----> columnIndex : ' + str(columnIndex)
+            print '-----> matrix vector : ' + str(matrixVector)
+            print '-----> vector: ' + str(vector)
+            print '-----> vectorial product: ' + str(vectorialProduct)
         
+        print '-----> vector dot matrix: ' + str(vectorDotMatrix)
+        '''
         return [self.sign(acum) for acum in vectorDotMatrix]
     
     def sign(self, value):
-        if value > 0:
+        if value >= 0:
             return 1
         return -1
        
-    def testWhatWasLearnt(self):
+    def testWhatWasLearnt(self, iteratedEpochs):
         print '--------------'
+        #print self.matrix
+        ''' Used for debugging
+        print 'Epoch: ' + str(iteratedEpochs)
         for learningData in self.parameters.learningSet:
             obtainedVector = np.dot(learningData.input, self.matrix)
             print 'input: ' + str(learningData.input)
             print str(learningData.expectedOutput) + ' --> expected'
             print str(obtainedVector) + ' --> obtained'
             print '--------------'
-            
+        '''    
+        
     def showEvolution(self):
         print 'matrix evolution, counter: ' + str(self.counter)
         print self.matrix
