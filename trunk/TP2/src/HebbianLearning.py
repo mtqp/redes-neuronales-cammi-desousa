f1a1c282e4f4aa7ce0bd2f46201278f209a25b71
marian.sabianaa <marian.sabianaa@6c33e5c6-7162-ad23-4877-bb0ec75854d9>
2014-05-28 13:09:11 +0000

    logging de xq no anda la hebbian learning

--- trunk/TP2/src/HebbianLearning.py
+++ trunk/TP2/src/HebbianLearning.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import random
 import time
+import matplotlib.pyplot as plt
 from MatrixVisualizer import MatrixVisualizer
 from pprint import pprint
 
@@ -19,27 +20,28 @@ class HebbianLearning:
     def algorithm(self, dataSet, rule): # n vector de R6: (a1,a2,a3,a4,a5,a6):
 
         epoch = 0
+
         while not self.endCondition.endConditionIsMet(epoch, self.etta):
             print 'Epoch: ' + str(epoch)
             for x in dataSet:
-                #print 'Corriendo para dato: ' + str(x)
-                y = self.multiplyVectorAndMatrix(x,self.matrix)
-                for j in range(0,self.m):
-                    for i in range(0,self.n):
-                        xAcum = np.zeros((self.n,1)).flatten() #Verificar que esto este funcionando correctamente.
-                        for k in range(1,rule(j,self.m)):
-                            xAcum[i] += y[k] * self.matrix[i][k]
-                        #print "(" + str(i) + "," + str(j) + ") y: " + str(y[j]) + "---- x[i] - xAcum[i]: " + str(x[i] - xAcum[i])
-                        self.deltaMatrix[i][j] = self.etta * y[j] * (abs(x[i]) - abs(xAcum[i]))
+                y = np.dot(x,self.matrix) #self.multiplyVectorAndMatrix(x,self.matrix)
+                for j in range(0, self.m):
+                    for i in range(0, self.n):
+                        xAcum = 0 #np.zeros((self.n, 1)).flatten()
+                        for k in range(0, rule(j, self.m)): #j+1 == sanger
+                            xAcum += (y[k] * self.matrix[i][k])
+                        self.deltaMatrix[i][j] = self.etta * y[j] * (x[i] - xAcum)
 
-                #self.visualizer.visualize(self.deltaMatrix)
-                self.matrix = np.add(self.matrix, self.deltaMatrix)
-
-                self.visualizer.visualize(self.matrix)
-
+                self.matrix += self.deltaMatrix
+                #self.matrix = np.add(self.matrix, self.deltaMatrix)
+                #self.visualizer.visualize(self.matrix)
             epoch = epoch + 1
 
-            self.printOrthogonalVectorCheck(self.matrix)
+            #self.printOrthogonalVectorCheck(self.matrix)
+
+        plt.imshow(self.matrix, interpolation="none", cmap=cm.gray )
+        plt.colorbar()
+        plt.show()
             #self.etta = 1.0/epoch
             #print 'Epoch: ' + str(epoch)
             #print 'Etta: ' + str(self.etta)
