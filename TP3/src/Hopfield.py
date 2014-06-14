37262468391e84fc2accce202d1672659c5683ac
martincammi <martincammi@6c33e5c6-7162-ad23-4877-bb0ec75854d9>
2014-06-14 15:33:48 +0000

    Algorimo Hopfield basico terminado

--- TP3/src/Hopfield.py
+++ TP3/src/Hopfield.py
@@ -1,30 +1,64 @@
 import numpy as np
 
+
 class Hopfield:
 
+    #s vector de activacion
+    #x vectores de aprendizaje (1 sola iteracion) son los patrones que se quieren memorizar
+
     def __init__(self, n):
-         self.n = n
-         self.matrix = np.zeros((self.n,self.n))
+        self.n = n
+        self.matrix = np.zeros((self.n,self.n))
 
-def training(self, dataSet):
-    for x in dataSet:
-        self.matrix += np.transpose(x)*x
-    self.matrix /= self.n
-    self.matrix -= self.matrix.diagonal(0)
-    return self.matrix
+    def training(self, dataSet):
+        for x in dataSet:
+            self.matrix += np.transpose(x)*x
+        self.matrix /= self.n
+        self.matrix -= self.matrix.diagonal(0)
+        return self.matrix
 
-def energy(self, s, matrix):
-    return -0.5 * ((s * matrix) * np.transpose(s))
+    def energy(self, s, matrix):
+        return -0.5 * ((s * matrix) * np.transpose(s))
 
-def activate(self, x, synch):
+    def activate(self, x, synch):
 
-    s = x
-    previousS = np.zeros((1,self.n))
+        s = x
+        previousS = np.zeros((1,self.n))
 
-    while previousS != s:
-        previousS[:] = s
+        while not (s == previousS).all():
+            previousS[:] = s
 
-        if synch:
-            s = sign()
+            if synch:
+                s = self.vectorSign( s * self.matrix)
+            else:
+                permIndexes = np.random.permutation(self.n)
 
-def sign(self)
+                for i in permIndexes:
+                    #print 'permIndexes: ' + str(permIndexes)
+                    #print 'i: ' + str(i)
+                    #print 'self.matrix[:,' + str(i) + ']: ' + str(self.matrix[:,i])
+                    #print 'result: ' + str(np.dot(s,self.matrix[:,i]))
+                    #print 's lengh' + str(s.shape)
+                    #print 's:' + str(s)
+                    #print 'matrix lengh' + str(self.matrix.shape)
+                    #print 'matrix:' + str(self.matrix)
+                    s[0, i] = self.sign( np.dot(s,self.matrix[:,i]))
+
+            energyValue = self.energy(s,self.matrix)
+            self.visualizeEnergy(energyValue,s)
+
+        return s
+
+    def vectorSign(self, vector):
+        return [self.sign(x) for x in vector]
+
+    def sign(self, item):
+        if item >= 0:
+            return 1
+        else:
+            return -1
+
+    def visualizeEnergy(self,energyValue, s):
+        #print "energyValue: " + str(energyValue)
+        #print "s value: " + str(s)
+        return None
--- TP3/src/main.py
+++ TP3/src/main.py
@@ -5,19 +5,40 @@ from DataSetCreator import DataSetCreator
 def main():
 
     #Initialize
-    n = 4
+    n = 10
     amountOfVectors = 5
-    firstBound = 0
-    endBound = 10
+    firstBound = -1
+    endBound = 1
     method = DataSetCreator.UNIFORM
-    hopfield = Hopfield()
+    hopfield = Hopfield(n)
 
     #DataSet Generation
     dataSetCreator = DataSetCreator(n)
     dataSetVectors = dataSetCreator.getRandomDataSetOfVectors(amountOfVectors, firstBound, endBound, method)
 
+    print 'random vectors:'
+    i = 0
+    for it in dataSetVectors:
+        print str(i) + ') ' + str(it)
+        i += 1
+
+
+    print 'matrix before:'
+    print hopfield.matrix
+
     #Learning
     hopfield.training(dataSetVectors)
+    print 'matrix after:'
+    print hopfield.matrix
+
+    #Activation
+    print 'Activation step:'
+    i = 0
+    for it in dataSetVectors:
+        s = hopfield.activate(it,False)
+        print str(i) + ') ' + str(s)
+        print 'Check: ' + str((dataSetVectors[i] == s).all())
+        i += 1
 
 
 if __name__ == "__main__":
