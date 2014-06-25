837e1954ffaae954fe5a06cb114234bf3c7ffa89
marian.sabianaa <marian.sabianaa@6c33e5c6-7162-ad23-4877-bb0ec75854d9>
2014-06-25 22:54:47 +0000

    stochastic algorithm for memory check

--- TP3/src/Hamming.py
+++ TP3/src/Hamming.py
@@ -1,7 +1,7 @@
 
 def distance(aVector, anotherVector):
     if not  len(aVector)==len(anotherVector):
-        raise Exception("Cannot calculate hammingDistance with vectors of diferrent sizes")
+        raise Exception("Cannot calculate hammingDistance with vectors of diferrent sizes: " + str(len(aVector)) + ' - ' + str(len(anotherVector)))
 
     distance = 0
     for i in range(0, len(aVector)):
